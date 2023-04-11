b276c45e3d4c51b46ffffad11d6b3363b883f43f
ChouAndy <chouandy625@gmail.com>
2023-04-11 16:44:26 +0800

    feat: add balancer v2 flash loan logic's api func

---
+++ .changeset/chatty-icons-fix.md
@@ -0,0 +1,5 @@
+---
+'@furucombo/composable-router-api': patch
+---
+
+add balancer v2 flash loan logic's api func
--- src/protocols/balancer-v2/flash-loan.ts
+++ src/protocols/balancer-v2/flash-loan.ts
@@ -1,7 +1,12 @@
 import { FlashLoanFields, FlashLoanFormData } from 'src/types';
+import { getProtocolTokenList } from 'src/api';
 import * as logics from '@furucombo/composable-router-logics';
 import { v4 as uuidv4 } from 'uuid';
 
 export function newFlashLoanFormData(fields: FlashLoanFields): FlashLoanFormData {
   return { id: uuidv4(), rid: logics.balancerv2.FlashLoanLogic.rid, fields };
+}
+
+export async function getFlashLoanTokenList(chainId: number): Promise<logics.balancerv2.FlashLoanLogicTokenList> {
+  return getProtocolTokenList(chainId, logics.balancerv2.FlashLoanLogic.rid);
 }
